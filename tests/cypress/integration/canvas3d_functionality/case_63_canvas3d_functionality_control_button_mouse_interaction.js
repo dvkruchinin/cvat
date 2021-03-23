36601173a9a6edc2005f399ff4b01fadb4d62000
dvkruchinin <dvkruchinin@gmail.com>
2021-03-23 17:30:39 +0300

    Cypress test. Control button mouse interaction.

--- tests/cypress/integration/canvas3d_functionality/case_63_canvas3d_functionality_control_button_mouse_interaction.js
+++ tests/cypress/integration/canvas3d_functionality/case_63_canvas3d_functionality_control_button_mouse_interaction.js
@@ -25,12 +25,12 @@ context('Canvas 3D functionality. Control button. Mouse interaction.', () => {
     ) {
         cy.get('.cvat-canvas3d-perspective').screenshot(screenshotNameBefore);
         cy.get(button).click();
-        testShowTooltipOnMousemoveToButton(button, expectedTooltipText);
+        testShowTooltipOnButton(button, expectedTooltipText);
         cy.get('.cvat-canvas3d-perspective').screenshot(screenshotNameAfter);
         compareImages(`${screenshotNameBefore}.png`, `${screenshotNameAfter}.png`);
     }
 
-    function testShowTooltipOnMousemoveToButton(button, expectedTooltipText) {
+    function testShowTooltipOnButton(button, expectedTooltipText) {
         cy.contains(expectedTooltipText).should('exist').and('be.visible');
         cy.get(button).trigger('mouseout');
         cy.contains(expectedTooltipText).should('not.exist');
@@ -38,7 +38,7 @@ context('Canvas 3D functionality. Control button. Mouse interaction.', () => {
 
     before(() => {
         cy.openTaskJob(taskName);
-        cy.get('.cvat-contextImage').should('be.visible');
+        cy.get('.cvat-contextImage-show').should('be.visible');
     });
 
     describe(`Testing case "${caseId}"`, () => {
@@ -48,6 +48,60 @@ context('Canvas 3D functionality. Control button. Mouse interaction.', () => {
                 'Arrow Up',
                 'before_click_uparrow',
                 'after_click_uparrow',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-arrow-directions-icons-down',
+                'Arrow Down',
+                'before_click_downarrow',
+                'after_click_downarrow',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-arrow-directions-icons-left',
+                'Arrow Left',
+                'before_click_leftarrow',
+                'after_click_leftarrow',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-arrow-directions-icons-right',
+                'Arrow Right',
+                'before_click_rightarrow',
+                'after_click_rightarrow',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-move-up',
+                'Alt+U',
+                'before_click_move_up',
+                'after_click_move_up',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-move-down',
+                'Alt+O',
+                'before_click_move_down',
+                'after_click_move_down',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-zoom-in',
+                'Alt+I',
+                'before_click_zoom_in',
+                'after_click_zoom_in',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-zoom-out',
+                'Alt+K',
+                'before_click_zoom_out',
+                'after_click_zoom_out',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-move-left',
+                'Alt+J',
+                'before_click_move_left',
+                'after_click_move_left',
+            );
+            testPerspectiveChangeOnButtonClick(
+                '.cvat-canvas3d-perspective-directions-icon-move-right',
+                'Alt+L',
+                'before_click_move_right',
+                'after_click_move_right',
             );
         });
     });
